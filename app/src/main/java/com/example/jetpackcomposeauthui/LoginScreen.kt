ca84219468c3964413491a1bcb3af30e35928258
deadcodee1 <anaahmedasaad.4@gmail.com>
2024-08-20 18:53:18 +0300

    Create Sign in and Sign up Screen

---
+++ app/src/main/java/com/example/jetpackcomposeauthui/LoginScreen.kt
@@ -0,0 +1,120 @@
+package com.example.jetpackcomposeauthui
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.jetpackcomposeauthui.components.CButton
+import com.example.jetpackcomposeauthui.components.CTextField
+import com.example.jetpackcomposeauthui.components.DontHaveAccountRow
+import com.example.jetpackcomposeauthui.ui.theme.AlegreyaFontFamily
+import com.example.jetpackcomposeauthui.ui.theme.AlegreyaSansFontFamily
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+    navController: NavHostController
+) {
+    // Defining state for email and password
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Surface(
+        color = Color.Black,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            // Content
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo2),
+                    contentDescription = null,
+                    Modifier.padding(bottom = 30.dp)
+                )
+                // Logo
+                Text(
+                    text = "Sign In",
+                    style = TextStyle(
+                        fontSize = 28.sp,
+                        fontFamily = AlegreyaFontFamily,
+                        fontWeight = FontWeight(500),
+                        color = Color.White
+                    ),
+                    modifier = Modifier.align(Alignment.Start)
+                )
+
+                Text(
+                    "Log in now to access your saved movies.",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontFamily = AlegreyaSansFontFamily,
+                        color = Color(0xB2FFFFFF)
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(bottom = 24.dp)
+                )
+
+                // Text Field
+                CTextField(
+                    hint = "Email Address",
+                    value = email,
+                    onValueChange = { email = it },  // Update email state
+                    keyboardType = KeyboardType.Email
+                )
+
+                CTextField(
+                    hint = "Password",
+                    value = password,
+                    onValueChange = { password = it },  // Update password state
+                    keyboardType = KeyboardType.Password
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                CButton(text = "Sign In")
+
+                DontHaveAccountRow(
+                    onSignupTap = {
+                        navController.navigate("signup")
+                    }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetpackcomposeauthui/SignupScreen.kt
@@ -0,0 +1,150 @@
+package com.example.jetpackcomposeauthui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.jetpackcomposeauthui.components.CButton
+import com.example.jetpackcomposeauthui.components.CTextField
+import com.example.jetpackcomposeauthui.components.DontHaveAccountRow
+import com.example.jetpackcomposeauthui.ui.theme.AlegreyaFontFamily
+import com.example.jetpackcomposeauthui.ui.theme.AlegreyaSansFontFamily
+
+@Composable
+fun SignupScreen(
+    navController: NavHostController
+) {
+    val fullName = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+
+    Surface(
+        color = Color.Black,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp)
+            ) {
+
+                // Logo
+                Image(
+                    painter = painterResource(id = R.drawable.logo2),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(bottom = 30.dp)
+                )
+
+                Text(
+                    text = "Sign Up",
+                    style = TextStyle(
+                        fontSize = 28.sp,
+                        fontFamily = AlegreyaFontFamily,
+                        fontWeight = FontWeight(500),
+                        color = Color.White
+                    ),
+                    modifier = Modifier.align(Alignment.Start)
+                )
+
+                Text(
+                    "Sign up now to access your saved movies.",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontFamily = AlegreyaSansFontFamily,
+                        color = Color(0xB2FFFFFF)
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(bottom = 24.dp)
+                )
+
+                // Text Fields with dynamic state
+                CTextField(
+                    hint = "Full Name",
+                    value = fullName.value,
+                    keyboardType = KeyboardType.Text,
+                    onValueChange = { fullName.value = it }
+                )
+
+                CTextField(
+                    hint = "Email Address",
+                    value = email.value,
+                    keyboardType = KeyboardType.Email,
+                    onValueChange = { email.value = it }
+                )
+
+                CTextField(
+                    hint = "Password",
+                    value = password.value,
+                    keyboardType = KeyboardType.Password,
+                    onValueChange = { password.value = it }
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                CButton(text = "Sign Up")
+
+                Row(
+                    modifier = Modifier.padding(top = 12.dp, bottom = 52.dp)
+                ) {
+                    Text(
+                        "Already have an account? ",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = AlegreyaSansFontFamily,
+                            color = Color.White
+                        )
+                    )
+
+                    Text(
+                        "Sign In",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = AlegreyaSansFontFamily,
+                            fontWeight = FontWeight(800),
+                            color = Color.Blue
+                        ),
+                        modifier = Modifier.clickable {
+                            navController.navigate("login")
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
+@Composable
+fun SignupScreenPreview() {
+    SignupScreen(rememberNavController())
+}
